7cd392104cb551318dd2890e08d7b3ca5ff6cbbf
AugustoCiuffoletti <augusto@di.unipi.it>
2021-03-29 15:34:18 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,22 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+interface miaInterfaccia {
+  f1: string,
+  f2: any
+}
+class miaClasse implements miaInterfaccia {
+  f1: string;
+  f2: number;
+  f3: boolean;
+  constructor() {
+    this.f1 = "";
+    this.f2 = 0;
+    this.f3 = false;
+  }
+}
+var X = new miaClasse();
+var a = "Hallo";
+X.f1 = a;
+X.f2 = 4;
+console.log(X);
